7fb0f79549e118babb3fd3478a6fb7a29678d8bf
cracker-hack <139023248+cracers-hack@users.noreply.github.com>
2023-07-20 08:43:21 +0330

    Add files via upload

---
+++ name_hacks.py
@@ -0,0 +1,54 @@
+from os import system
+from colorama import Fore
+from time import sleep
+
+system("clear")
+
+print("Hello World")
+print("")
+
+tem = (f"""
+                        .xUHWH!! !!?MRDARK:.
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRSALMAN!
+                     ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~
+
+
+""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+print("")
+code = input(Fore.MAGENTA+f"""
+(1) code filtering account
+(2) code filtering groop
+(3) code filtering chanell
+code filtering >>>""")
+print("")
+
+print(Fore.GREEN+"loding")
+
+if code == "1":
+  print(Fore.RED+"1.3.5.7///f///f/.2.4.6.8.9.6.4.2.1.1.1.3.5.7.9.9.7.4.3.2.1")
+  
+if code == "2":
+  print(Fore.RED+"1.3.5.7//f//y//1.3.6.3.5.7.8.9.7.6.4.3.2.1.1.3.5.7.8.9.0")
+
+if code == "3":
+  print(Fore.RED+"1.3.6.8.//f//g//1.3.6.7.9.0.6.4.2.1.1.1.3.4.6.7.8.4.3.2.1")
